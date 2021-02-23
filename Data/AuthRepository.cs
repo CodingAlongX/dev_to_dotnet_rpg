05f766736ba88aa3b6886a31b826773c1e7d8f66
S. M. JAHANGIR <smjxpro@gmail.com>
2021-02-24 01:36:58 +0600

    added user registration method to auth repository
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Threading.Tasks;
 using dev_to_dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dev_to_dotnet_rpg.Data
 {
-    public class AuthRepository:IAuthRepository
+    public class AuthRepository : IAuthRepository
     {
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+
+            if (await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists";
+
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            response.Data = user.Id;
+
+            return response;
         }
 
         public Task<ServiceResponse<string>> Login(string username, string password)
@@ -15,9 +43,26 @@ namespace dev_to_dotnet_rpg.Data
             throw new System.NotImplementedException();
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+            {
+            }
         }
     }
 }
